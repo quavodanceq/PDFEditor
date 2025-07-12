c8afebc432f8b9dd05fbff5c59e2b3d4c6a85714
Kuat <80914126+quavodanceq@users.noreply.github.com>
2025-07-12 12:47:41 +0500

    Initial Commit

---
+++ PdfEditor/PdfEditorApp.swift
@@ -0,0 +1,20 @@
+//
+//  PdfEditorApp.swift
+//  PdfEditor
+//
+//  Created by Куат Оралбеков on 12.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct PdfEditorApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
